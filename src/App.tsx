313d383dc065c2c36dc200bd4c02fe0c9b9f5b2d
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-11-17 15:50:27 -0300

    add button list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 
 
 function App() {
+  const [list, setList] = useState(['Tiago', 'Lucas', 'Pedro'])
+
+  const addToList = () => setList(state => [...state, 'New'])
 
   return (
-    <h1 className="App">Hello World</h1>
+    <>
+      <button onClick={addToList}>Add</button>
+      <ul>
+        {list.map(item=> <li key={item}>{item}</li>)}
+      </ul>
+    </>
   )
 }
 
