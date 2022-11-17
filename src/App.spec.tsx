4d3594d8dd292d54f9af09c1f9ca3daba305870a
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-11-17 16:08:54 -0300

    button click

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,8 +1,22 @@
 import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
-test('sum', () => {
-    const { getByText } = render(<App />)
+describe('App Component', ()=>{
+    it('Should render list items', ()=>{
+        const { getByText} = render(<App />)
+        
+        expect(getByText('Tiago')).toBeInTheDocument()
+        expect(getByText('Lucas')).toBeInTheDocument()
+        expect(getByText('Pedro')).toBeInTheDocument()
+    })
     
-    expect(getByText('Hello World')).toHaveAttribute('class', 'App')
+    it('Should be able to add new item to the list', async ()=> {
+        const { getByText, debug } = render(<App />)
+        const addButton = getByText('Add')
+
+        await userEvent.click(addButton)
+
+        expect(getByText('New')).toBeInTheDocument()
+    })
 })
